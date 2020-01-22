8d78fcf7e99472149cc57380d16cc6e5196c988b
kumarsolanki <kumarsolanki.003@gmail.com>
2020-01-22 11:55:04 +0530

    Exception Propagation and throw/throws

---
+++ src/CustomException.java
@@ -0,0 +1,30 @@
+class Simple {
+    void m()
+    {
+        int data = 50 / 0; // unchecked exception occurred
+        // exception propagated to n()
+    }
+
+    void n()
+    {
+        m();
+        // exception propagated to p()
+    }
+
+    void p()
+    {
+        try {
+            n(); // exception handled
+        }
+        catch (Exception e) {
+            System.out.println("Exception handled");
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Simple obj = new Simple();
+        obj.p();
+        System.out.println("Normal flow...");
+    }
+}
---
+++ src/com/solanki/exception/ExceptionProgation.java
@@ -0,0 +1,41 @@
+package com.solanki.exception;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ExceptionProgation {
+
+    ExceptionProgation() throws IOException {
+
+        //throw new IOException("hello");
+    }
+
+    void m1() throws Exception {
+
+    }
+    void m2() throws IOException{
+        try {
+            m1();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    void m3() {
+        throw  new RuntimeException("RunTime Exception has occured..");
+    }
+    public static void main(String[] args) {
+        ExceptionProgation exceptionProgation = null;
+        try {
+            exceptionProgation    = new ExceptionProgation();
+        }
+        catch (Exception e){
+        System.out.println("Exception occured");
+        }
+        try{
+        exceptionProgation.m3();}
+        catch(Exception e){
+            System.out.println("Exception occured for m3");
+        }
+
+    }
+}
